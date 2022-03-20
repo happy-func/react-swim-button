4b5ed1b0b275356841a0fe0ae3c9f82059c99679
happy-func <stevenhanth@gmail.com>
2022-03-20 21:27:21 +0800

    fix github pages

--- .umirc.ts
+++ .umirc.ts
@@ -12,8 +12,35 @@ export default defineConfig({
   },
   hash: true,
   webpack5: {},
+  // 通过 [webpack-chain](https://github.com/mozilla-neutrino/webpack-chain) 的 API 修改 webpack 配置。
+  chainWebpack(memo, { env, webpack, createCSSRule }) {
+    memo.cache = {
+      type: 'filesystem',
+      name: 'dumi',
+      buildDependencies: {
+        config: [__filename],
+      },
+      store: 'pack',
+    };
+    memo.plugins.delete('friendly-error');
+    memo.plugins.delete('copy');
+  },
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
+  styles: [
+    `
+      #root .__dumi-default-menu-header p {
+        display:none;
+      }
+      #root .__dumi-default-menu-header h1 {
+        font-size: 24px;
+        margin: 16px auto;
+      }
+      #root .__dumi-default-mobile-demo-layout{
+        padding:0;
+      }
+    `,
+  ],
 });
